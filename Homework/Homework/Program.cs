2d4a41931b0af0ed762d4d353509418e7b5f4564
Khokhlov Oleksii <alekseu87w@gmail.com>
2023-11-26 21:00:42 +0200

    O.Khokhlov_dz1
    CRUD

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework
+{
+    internal abstract class Program
+    {
+        private static List<Person> _people = new List<Person>();
+        static void Main()
+        {
+            PersonControllers.Dict[6](ref _people);
+            while (true)
+            {
+                Console.WriteLine("Select the desired option: ");
+                Int32.TryParse(Console.ReadLine(), out var key);
+                if (PersonControllers.Dict.Count <= key)
+                    PersonControllers.Dict[0](ref _people);
+                else
+                    PersonControllers.Dict[key](ref _people);
+            }
+        }
+    }
+}
